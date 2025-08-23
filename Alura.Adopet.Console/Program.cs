b5e303b118e3f13cb10b1ce87febebe976a60376
Denisio Rodrigues <denisio@ymail.com>
2025-08-23 10:42:22 -0300

    Importacao de arquivo pet

--- Alura.Adopet.Console/Program.cs
+++ Alura.Adopet.Console/Program.cs
@@ -12,37 +12,8 @@ try
     switch (comando)
     {
         case "import":
-            List<Pet> listaDePet = new List<Pet>();
-
-            string caminhoDoArquivoParaImportacao = args[1];
-            using (StreamReader sr = new StreamReader(caminhoDoArquivoParaImportacao))
-            {
-                while (!sr.EndOfStream)
-                {
-                    // separa linha usando ponto e vírgula
-                    string[] propriedades = sr.ReadLine().Split(';');
-                    // cria objeto Pet a partir da separação
-                    Pet pet = new Pet(Guid.Parse(propriedades[0]),
-                      propriedades[1],
-                      TipoPet.Cachorro
-                     );
-
-                    Console.WriteLine(pet);
-                    listaDePet.Add(pet);
-                }
-            }
-            foreach (var pet in listaDePet)
-            {
-                try
-                {
-                    var resposta = await CreatePetAsync(pet);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
-            Console.WriteLine("Importação concluída!");
+            var importar = new Importar();
+            await importar.ArquivoPetAsync(caminhoDoArquivoParaImportacao: args[1].Trim());
             break;
         case "help":
             Console.WriteLine("Lista de comandos.");
