cb38afa7e2e66a5035bc18430e490ecf1c72d16f
Denisio Rodrigues <denisio@ymail.com>
2025-08-24 13:10:54 -0300

    Removendo duplicidade da documentacao com refleactions

--- Alura.Adopet.Console/Ajuda.cs
+++ Alura.Adopet.Console/Ajuda.cs
@@ -1,20 +1,32 @@
 using System;
+using System.Reflection;
 
 namespace Alura.Adopet.Console;
 
 [DocComando(comando: "help", instrucao: "comando que exibe informações de ajuda.ao ")]
 public class Ajuda
 {
+    private Dictionary<string, DocComando> docs;
+
+    public Ajuda()
+    {
+        docs = Assembly.GetExecutingAssembly()
+            .GetTypes()
+            .Where(t => t.GetCustomAttributes<DocComando>().Any())
+            .Select(t => t.GetCustomAttribute<DocComando>())
+            .ToDictionary(t => t.Instrucao);
+    }
+    
     public void Executar(string[] parametrosInformados) 
     {
         System.Console.WriteLine("Lista de comandos.");
         // se não passou mais nenhum argumento mostra help de todos os comandos
         if (parametrosInformados.Length == 1)
         {
-            System.Console.WriteLine("adopet help <parametro> ous simplemente adopet help comando que exibe informações de ajuda dos comandos.");
             System.Console.WriteLine("Adopet (1.0) - Aplicativo de linha de comando (CLI).");
             System.Console.WriteLine("Realiza a importação em lote de um arquivos de pets.");
             System.Console.WriteLine("Comando possíveis: ");
+            System.Console.WriteLine("adopet help <parametro> ous simplemente adopet help comando que exibe informações de ajuda dos comandos.");
             System.Console.WriteLine($"\t adopet import <arquivo> comando que realiza a importação do arquivo de pets.");
             System.Console.WriteLine($"\t adopet show   <arquivo> comando que exibe no terminal o conteúdo do arquivo importado." + "\n\n\n");
             System.Console.WriteLine("Execute 'adopet.exe help [comando]' para obter mais informações sobre um comando." + "\n\n");
