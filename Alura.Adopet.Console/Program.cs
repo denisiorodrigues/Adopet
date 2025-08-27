b9330ebf2fd49d39fc79bd24494c7d32a93abdcf
Denisio Rodrigues <denisio@ymail.com>
2025-08-27 20:31:50 -0300

    aplicando polimorfismo na execucao dos comandos

--- Alura.Adopet.Console/Program.cs
+++ Alura.Adopet.Console/Program.cs
@@ -2,31 +2,25 @@
 
 Console.ForegroundColor = ConsoleColor.Green;
 
+IDictionary<string, IComando> comandos = new Dictionary<string, IComando>()
+{
+    {"import", new Importar()},
+    {"help", new Ajuda()},
+    {"show", new Exibir()},
+    {"list", new Listar()},
+};
+
 try
 {
-    string comando = args[0].Trim(); 
-    
-    switch (comando)
+    string comando = args[0].Trim();
+    if (comandos.ContainsKey(comando))
     {
-        case "import":
-            var importar = new Importar();
-            await importar.ExecutarAsync(args);
-            break;
-        case "help":
-            var ajuda = new Ajuda();
-            await ajuda.ExecutarAsync(args);
-;           break;
-        case "show":
-            var exibir = new Exibir();
-            await exibir.ExecutarAsync(args);
-            break;
-        case "list":
-            var listar = new Listar();
-            await listar.ExecutarAsync(args);
-            break;
-        default:
-            Console.WriteLine("Comando inválido!");
-            break;
+        var cmd = comandos[comando];
+        await cmd.ExecutarAsync(args);
+    }
+    else
+    {
+        Console.WriteLine("Comando inválido!");
     }
 }
 catch (Exception ex)
