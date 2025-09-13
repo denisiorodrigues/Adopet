207f8f575261f3521488b64ec27ace0349dfb20f
Denisio Rodrigues <denisio@ymail.com>
2025-09-13 10:25:37 -0300

    Separar o teste de unidade do de integração

--- Alura.Adopet.Test/ImportTest.cs
+++ Alura.Adopet.Test/ImportTest.cs
@@ -1,27 +1,12 @@
-﻿using Alura.Adopet.Console.Comandos;
-using Alura.Adopet.Console.Modelo;
-using Alura.Adopet.Console.Servicos;
-using Alura.Adopet.Console.Utilitarios;
-using Moq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Alura.Adopet.Test;
-
-public class ImportTest
+namespace Alura.Adopet.Test
 {
-    [Fact]
-    public async Task QuandoAPIIEstaNoArDeveRetornarLista()
+    internal class ImportTest
     {
-        var leitorDeArquivo = new Mock<LeitorDeArquivos>(MockBehavior.Default, It.IsAny<string>());
-        var pet = new Pet(new Guid(), "Lima", TipoPet.Cachorro);
-        leitorDeArquivo.Setup(_ => _.RealizarLeitura()).Returns(new List<Pet>() { pet });
-
-        var httpClientPet = new HttpClientPet(new AdopetAPIClientFactory().CreateClient("adopet"));
-        var importar = new Importar(httpClientPet, leitorDeArquivo.Object);
-        var args = new string[] { "import", "animais.csv" };
-
-        await importar.ExecutarAsync(args);
-        var listaPets = await httpClientPet.ListPetsAsync();
-
-        Assert.NotNull(listaPets);
     }
 }
