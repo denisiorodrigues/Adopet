9a4bfb82df96fabf3eb526d37343ad4067082ff8
Denisio Rodrigues <denisiorodrigues@digix.com.br>
2025-09-24 08:22:46 -0300

    Teste unitario do exibir

---
+++ Alura.Adopet.Test/DataBuilder/PetBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Alura.Adopet.Console.Modelo;
+
+namespace Alura.Adopet.Test.DataBuilder
+{
+    public static class PetBuilder
+    {
+        public static List<Pet> GetMock()
+        { 
+            return new List<Pet>()
+            {
+                new Pet( Guid.NewGuid(), "Lima Limão", TipoPet.Cachorro),
+                new Pet( Guid.NewGuid(), "Spike", TipoPet.Cachorro)
+            };
+        }
+    }
+}
---
+++ Alura.Adopet.Test/ExibirTest.cs
@@ -0,0 +1,45 @@
+﻿using Alura.Adopet.Console.Comandos;
+using Alura.Adopet.Console.Modelo;
+using Alura.Adopet.Console.Utilitarios;
+using Alura.Adopet.Test.DataBuilder;
+using Moq;
+
+namespace Alura.Adopet.Test;
+
+public class ExibirTest
+{
+    Mock<LeitorDeArquivos> LeitorDeArquivoMock;
+
+    public ExibirTest()
+    {
+        var listaDePets = PetBuilder.GetMock();
+        LeitorDeArquivoMock = LeitorDeArquivoMockBuilder.GetMock(listaDePets);
+    }
+
+    [Fact]
+    public async Task QuandoExibirDeveRetonarUmResultadoDeSucessso()
+    {
+        var exibir = new Exibir(LeitorDeArquivoMock.Object);
+        
+        var result = await exibir.ExecutarAsync();
+        var petComsuCesso = (SucessoComPet)result.Successes.First();
+
+
+        Assert.True(result.IsSuccess);
+        Assert.NotNull(petComsuCesso);
+        Assert.Equal(2, petComsuCesso.Data.Count());
+    }
+
+    [Fact]
+    public async Task QuandoExibirDeveRetornarFalha()
+    {
+        LeitorDeArquivoMock.Setup(l => l.RealizarLeitura()).Throws(new Exception());
+        var exibir = new Exibir(LeitorDeArquivoMock.Object);
+
+        var result = await exibir.ExecutarAsync();
+
+        Assert.True(result.IsFailed);
+        Assert.NotEmpty(result.Errors);
+        Assert.Equal("O camndo Exibição(show) falhou!", result.Errors.First().Message);
+    }
+}
